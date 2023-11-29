7a8399bb5ae344092e9cc750917738d7cf97d2a4
fractalpete <105214155+fractalpete@users.noreply.github.com>
2023-11-29 10:20:44 +0000

    js fix

--- js/script.js
+++ js/script.js
@@ -3,35 +3,33 @@ function openModal(imageSrc) {
     var modalImg = document.getElementById("img01");
     var captionText = document.getElementById("caption");
 
-    modal.style.display = "flex"; // Changed to 'flex' for centering
+    modal.style.display = "flex";
+    modalImg.src = ""; // Reset the image source
     modalImg.src = imageSrc;
     captionText.innerHTML = imageSrc.split('/').pop();
 
     modalImg.onload = function() {
         if (modalImg.naturalWidth > modalImg.naturalHeight) {
-            // Landscape
             modalImg.style.height = 'auto';
             modalImg.style.width = '100%';
         } else {
-            // Portrait or square
             modalImg.style.width = 'auto';
             modalImg.style.height = '100%';
         }
     };
-
-    // When the user clicks anywhere outside of the modalImg, close it
-    window.onclick = function(event) {
-        if (event.target === modal) {
-            modal.style.display = "none";
-        }
-    }
 }
 
-// Function to update the current year in the footer
+// Separated event listener for closing the modal
+window.onclick = function(event) {
+    var modal = document.getElementById("myModal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
+
 function updateYear() {
     var currentYear = new Date().getFullYear();
     document.getElementById('currentYear').textContent = currentYear;
 }
 
-// Call the function when the page loads
 updateYear();
