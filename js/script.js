2ccc13636152e83d8916f6ecd0b3c2a3ac29d5b1
fractalpete <105214155+fractalpete@users.noreply.github.com>
2023-11-30 12:28:56 +0000

    tweaks

--- js/script.js
+++ js/script.js
@@ -23,7 +23,6 @@ function openModal(imageSrc) {
         }, 300);
     };
 
-    // Touch event for mobile devices
     closeButton.ontouchstart = function() {
         modal.style.display = "none";
         modalOpen = false;
@@ -34,7 +33,6 @@ function openModal(imageSrc) {
     };
 }
 
-// Separated event listener for closing the modal by clicking outside
 window.onclick = function(event) {
     var modal = document.getElementById("myModal");
     if (event.target === modal) {
@@ -54,26 +52,113 @@ function updateYear() {
 
 updateYear();
 
-// Get all gallery images
+var modalOpen = false; // Declare this variable outside the function
+var cooldown = false; // Add a cooldown variable
+
+function openModal(imageSrc) {
+    if (modalOpen || cooldown) return; // Prevent opening if modal is already open or in cooldown
+
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    var closeButton = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "flex";
+    modalImg.src = imageSrc;
+    captionText.innerHTML = imageSrc.split('/').pop();
+    modalOpen = true; // Set modal as open
+
+    closeButton.onclick = function() {
+        modal.style.display = "none";
+        modalOpen = false;
+        cooldown = true; // Start cooldown
+        setTimeout(function() {
+            cooldown = false; // End cooldown after 300 milliseconds
+        }, 300);
+    };
+
+    closeButton.ontouchstart = function() {
+        modal.style.display = "none";
+        modalOpen = false;
+        cooldown = true; // Start cooldown
+        setTimeout(function() {
+            cooldown = false; // End cooldown after 300 milliseconds
+        }, 300);
+    };
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById("myModal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+        modalOpen = false;
+        cooldown = true; // Start cooldown
+        setTimeout(function() {
+            cooldown = false; // End cooldown after 300 milliseconds
+        }, 300);
+    }
+};
+
+function updateYear() {
+    var currentYear = new Date().getFullYear();
+    document.getElementById('currentYear').textContent = currentYear;
+}
+
+updateYear();
+
 const galleryImages = document.querySelectorAll('.gallery-item img');
 
-// Add mouseenter event listener to each image
 galleryImages.forEach((image) => {
-    image.addEventListener('mouseenter', () => {
-        // Reduce opacity for all other images
+    // Initial style setup
+    image.style.border = '1px solid transparent';
+    image.style.zIndex = 0; // Default z-index for non-hovered images
+    image.style.position = 'static'; // Default position
+    image.style.transition = 'transform 1s ease-out, opacity 0.5s, border-color 0.5s ease-out';
+
+    // Function to handle image movement
+    const moveImageToCursor = (event) => {
+        const rect = image.getBoundingClientRect();
+        const imageCenterX = rect.left + rect.width / 2;
+        const imageCenterY = rect.top + rect.height / 2;
+
+        let translateX = (event.clientX - imageCenterX) * 0.9;
+        let translateY = (event.clientY - imageCenterY) * 0.8;
+
+        image.style.transform = `scale(2) translate(${translateX}px, ${translateY}px)`;
+    };
+
+    image.addEventListener('mouseenter', (e) => {
+        moveImageToCursor(e);
+        image.addEventListener('mousemove', moveImageToCursor);
+
+        image.style.borderColor = 'black';
+        image.style.zIndex = 10; // Higher z-index on hover
+        image.style.position = 'relative'; // Make position relative on hover
+
+        // Dim and shrink other images
         galleryImages.forEach((otherImage) => {
             if (otherImage !== image) {
-                otherImage.style.opacity = 0.2;
+                otherImage.style.opacity = 0.4;
+                otherImage.style.transform = 'scale(0.1)';
             }
         });
     });
 
-    // Add mouseleave event listener to each image to reset opacity
     image.addEventListener('mouseleave', () => {
-        // Reset opacity for all images
+        image.removeEventListener('mousemove', moveImageToCursor);
+
+        // Reset image styles
+        image.style.opacity = 1;
+        image.style.transform = 'none';
+        image.style.borderColor = 'transparent';
+        image.style.zIndex = 0; // Reset z-index
+        image.style.position = 'static'; // Reset position to static
+        image.style.transition = 'transform 0.5s ease-out, opacity 0.3s, border-color 1s ease-out';
+
+        // Reset styles for other images
         galleryImages.forEach((otherImage) => {
             otherImage.style.opacity = 1;
+            otherImage.style.transform = 'scale(1)';
         });
     });
 });
-
