1020c524e946d9fc0a2ebf09610a76096da45353
fractalpete <105214155+fractalpete@users.noreply.github.com>
2023-11-30 11:11:51 +0000

    js updates with border and transalation

--- js/script.js
+++ js/script.js
@@ -55,23 +55,58 @@ updateYear();
 const galleryImages = document.querySelectorAll('.gallery-item img');
 
 galleryImages.forEach((image) => {
+
+    // Set initial border as transparent for all images
+    image.style.border = '2px solid transparent';
+    image.style.transition = 'border-color 0.5s ease-out';
+
     image.addEventListener('mouseenter', () => {
-        image.style.transition = 'opacity 0.5s, transform 0.5s';
+        const rect = image.getBoundingClientRect();
+        const imageCenterX = rect.left + rect.width / 2;
+        const imageCenterY = rect.top + rect.height / 2;
+
+        const windowCenterX = window.innerWidth / 2;
+        const windowCenterY = window.innerHeight / 2;
+
+        let translateX = windowCenterX - imageCenterX;
+        let translateY = windowCenterY - imageCenterY;
+
+        // Limit the translation to a maximum of 100 pixels
+        translateX = Math.max(Math.min(translateX, 100), -100);
+        translateY = Math.max(Math.min(translateY, 100), -100);
+
+        image.style.transition = 'opacity 0.5s, transform 4s ease-out'; // Increased duration and added ease-out
         image.style.opacity = 1;
-        image.style.transform = 'scale(1.5)'; // Scale up by 1.5x
+        image.style.transform = `scale(2) translate(${translateX}px, ${translateY}px)`; 
+
+         // Change border color to black on hover and z-index
+        image.style.borderColor = 'black';
+        image.style.zIndex = 10; // Ensure it's above other images
+
+         setTimeout(() => {
+             image.style.transition += ', border-opacity 0.5s ease-out';
+             image.style.borderOpacity = 1; // Transition to full opacity
+         }, 0); // Timeout to ensure the transition is applied
 
         galleryImages.forEach((otherImage) => {
             if (otherImage !== image) {
-                otherImage.style.opacity = 0.2;
-                otherImage.style.transform = 'scale(0.1)'; // Scale down other images to 0.1x
+                otherImage.style.opacity = 0.4;
+                otherImage.style.transform = 'scale(0.2)';
             }
         });
+
     });
 
     image.addEventListener('mouseleave', () => {
+
+// Reset border color to transparent and reset z-index
+image.style.borderColor = 'transparent';
+image.style.zIndex = 0; // Reset z-index
+
         galleryImages.forEach((otherImage) => {
             otherImage.style.opacity = 1;
-            otherImage.style.transform = 'scale(1)'; // Reset scale to original
+            otherImage.style.transition = 'opacity 0.5s, transform 2s ease-out';
+            otherImage.style.transform = 'none';
         });
     });
 });
